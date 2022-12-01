297b4bf8f4a7a0b451c4a8202a1263593487ef49
unknown <body5401@gmail.com>
2022-12-01 11:57:39 +0200

    some new displays added

--- FawryPaymentSystem/src/AdminMainMenu.java
+++ FawryPaymentSystem/src/AdminMainMenu.java
@@ -6,6 +6,10 @@ public class AdminMainMenu extends Display{
         System.out.println("2- Add Discounts to the system");
         System.out.println("3- List Refund requests");
         this.option=sc.nextInt();
+        while(this.option<1||this.option>3){
+            System.out.print("Error, Enter a valid number please: ");
+            this.option=sc.nextInt();
+        }
         return this.option;
     }
 }
